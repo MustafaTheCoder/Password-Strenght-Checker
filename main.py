438acc12cbe45b55ed610e618c9356ae5285040a
MustafaTheCoder <73116655+MustafaTheCoder@users.noreply.github.com>
2021-09-19 22:16:22 +0500

    Edited Algorithm

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import string
 
-
-
 while True:
-    user_inp = input("Enter Password: ")
-    s1 = string.ascii_lowercase #the string module funtion that gives us all the lower case letters
-    s2 = string.ascii_uppercase # the string module funtion that gives us the uppercase letters
-    s3 = string.digits # the string module function that gives us all the digits
-    s4 = string.punctuation # the function of string module that returns punctuations 
-    if s1[0:5] or s2[0:5] or s3[0:5] or s4[0:5] in user_inp:
+    user_inp = str(input("Enter Password: "))
+
+    s1 = string.ascii_lowercase 
+    s2 = string.ascii_uppercase 
+    s3 = string.digits 
+    s4 = string.punctuation 
+
+
+
+    if s1[0:2] and s2[0:2] and s3[0:2] and s4[0:2] in user_inp:
         print("Password Strong!")
+    elif len(user_inp) == 8 or len(user_inp) > 8:
+        print("Password Strong!")    
     else:
-        print("Password Weak!")    
+        print("Password Weak!")   
+
